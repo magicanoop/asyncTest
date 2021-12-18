70650aaacebcd134f2e0d218f0e2ed5a8a1f252d
Anoop <anoopsurendran@neyyar.in>
2021-12-18 12:23:32 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+use Spatie\Async\Pool;
+
+$pool = Pool::create();
+
+foreach ($things as $thing) {
+    $pool->add(function () use ($thing) {
+        // Do a thing
+    })->then(function ($output) {
+        // Handle success
+    })->catch(function (Throwable $exception) {
+        // Handle exception
+    });
+}
+
+$pool->wait();
